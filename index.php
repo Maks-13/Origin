f407fbcf766ba45a156018163fbaaf8a37d68de5
Maksim <max01_13@mail.ru>
2020-10-01 19:10:48 +0300

    init commit1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Моя страничка </title>
+<link rel = "stylesheet" href = "mycss.css>
+<link rel = "shortcut icon" href = "favicon.ico">
+</head>
+<body>
+<div class = "mydiv1">
+<?php
+echo "Дана трапеция с основаниями: a = 4, b = 6 <br/>";
+echo "Длина средней линии трапеции: ";
+echo (6 + 4)/2; 
+?>
+</div>
+</body>
+</html>
